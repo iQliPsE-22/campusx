7e6056bf845aad3b2df1cb37cad9e3ce8485c808
Deepak Kumar <deepaklg02@gmail.com>
2024-11-05 22:37:50 +0530

    Responsive in login and signup

--- app/login/page.js
+++ app/login/page.js
@@ -14,7 +14,7 @@ const Page = () => {
     <>
       <section className="bg-black h-full pt-8 p-4 lg:p-12 min-h-dvh">
         <Headline />
-        <div className="flex flex-col lg:flex-row items-center justify-around">
+        <div className="min-h-dvh flex flex-col lg:flex-row items-center justify-center lg:justify-around">
           <div className="w-4/5 lg:w-1/2 flex justify-center p-2 lg:p-8 ">
             <Image
               src={loginimg}
