d65acca3a5331b29197a2ae9d2f45b3d0038e813
Deepak Kumar <deepaklg02@gmail.com>
2025-01-14 15:31:26 +0530

    Error resolved

--- app/page.js
+++ app/page.js
@@ -1,7 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button"; // Adjust the path as necessary
 import Header from "../components/Header"; // Adjust the path as necessary
-import Navbar from "./../components/Navbar";
 import Hero from "./../components/Hero/Hero";
 export default function Home() {
   return (
